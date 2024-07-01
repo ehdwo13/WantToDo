5830ce65e5736a89abbfc8403762974558be45a3
ehdwo13 <ehdwo13@gmail.com>
2024-07-01 18:10:29 +0900

    js

--- calculator/index.js
+++ calculator/index.js
@@ -1,6 +1,3 @@
-const button = document.querySelectorAll('.btn');
-const operator = document.querySelectorAll('.operator');
-const clear = document.querySelector('.clear');
 const display = document.getElementById('display');
 
 let prevVal = 0;
@@ -9,3 +6,17 @@ document.querySelector('.clear').addEventListener('click',()=>{
     prevVal = 0;
     display.innerText = 0;
 })
+
+document.querySelector('.buttons').addEventListener('click',(e)=>{
+    let btnVal = e.target.innerText;
+    switch(btnVal){
+        case "+" : display.innerText = prevVal + parseInt(btnVal); break;
+        case "-" : display.innerText = prevVal - parseInt(btnVal); break;
+        case "*" : display.innerText = prevVal * parseInt(btnVal); break;
+        case "/" : display.innerText = prevVal / parseInt(btnVal); break;
+        case "=" : display.innerText = prevVal; break;
+        default : prevVal = parseInt(btnVal);
+    }
+    display.innerText = btnVal;
+})
+
