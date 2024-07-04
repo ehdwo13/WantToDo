60d7b446046de4108566b90bf0a59a84d33fdda4
ehdwo13 <ehdwo13@gmail.com>
2024-07-04 18:34:28 +0900

    20240704

---
+++ JavaTalk/JavaTalk/src/config/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/javatalkdb";
+    private static final String USER = "local";
+    private static final String PASSWORD = "mysql";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC 드라이버 로드 실패", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ JavaTalk/JavaTalk/src/user/UserVO.java
@@ -0,0 +1,39 @@
+package user;
+
+public class UserVO {
+	
+	private String userName;
+	private String nickName;
+	private String password;
+	
+	
+	public UserVO() {}
+	
+	public UserVO(String userName, String nickName, String password) {
+		this.userName = userName;
+		this.nickName = nickName;
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "UserVO [userName=" + userName + ", nickName=" + nickName + ", password=" + password + "]";
+	}
+}
