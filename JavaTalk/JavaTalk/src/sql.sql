dacd19858649ad94a68eacfb66c91b8c5704cac3
ehdwo13 <ehdwo13@gmail.com>
2024-07-08 21:56:06 +0900

    add friend sql query & after logged in menu

--- JavaTalk/JavaTalk/src/sql.sql
+++ JavaTalk/JavaTalk/src/sql.sql
@@ -4,3 +4,11 @@ create table user(
 	password varchar(250) not null,
 	nick_name varchar(100) not null
 );
+
+create table friends(
+	user_id int,
+	friend_id int,
+	PRIMARY KEY (user_id, friend_id),
+    FOREIGN KEY (user_id) REFERENCES user(count),
+    FOREIGN KEY (friend_id) REFERENCES user(count)
+);
